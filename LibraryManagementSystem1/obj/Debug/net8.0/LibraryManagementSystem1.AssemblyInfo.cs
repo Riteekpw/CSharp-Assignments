7a92411d1c0a5b4c5d84306784730ce51a0e838b
Riteekpw <riteekchopkar@gmail.com>
2025-02-21 17:11:02 +0530

    Stop tracking .vs and obj directories

--- LibraryManagementSystem1/obj/Debug/net8.0/LibraryManagementSystem1.AssemblyInfo.cs
+++ LibraryManagementSystem1/obj/Debug/net8.0/LibraryManagementSystem1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e12419b580024ed90438020e3ccb54fd9d50be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c715e0ca978758bac34f4487ebae9c37ec7c61")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
